8ec228eea8c7b7b1f3eaac78e6f240b861fc9809
Yashwanthssy <yashwanthssy283@gmail.com>
2026-04-20 16:14:22 +0530

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'resume-analyzer-backend-production-2dc6.up.railway.app'
+  apiUrl: 'https://resume-analyzer-backend-production-2dc6.up.railway.app'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'resume-analyzer-backend-production-2dc6.up.railway.app'
+  apiUrl: 'https://resume-analyzer-backend-production-2dc6.up.railway.app'
 };
